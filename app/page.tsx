15e50dd44034958c19ad002e242abc07642182e4
ZapChainDev <papelirinjohnmarkdote@gmail.com>
2026-03-27 11:07:19 +0800

    Change image to pic for road.png

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation";
 
-// No UI served. All traffic should go directly to /outreach-preview.jpg
+// No UI served. All traffic should go directly to /pic for road.png
 export default function Page() {
   notFound();
 }
